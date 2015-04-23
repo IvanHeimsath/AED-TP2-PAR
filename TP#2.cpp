c657f1e644a00c7c4459d94b214b62cb97249688
Ivan Heimsath <ivan_heimsath@hotmail.com>
2015-04-23 00:20:44 -0300

    Create TP#2.cpp

---
+++ TP#2.cpp
@@ -0,0 +1,22 @@
+/*
+	Name: TP#2-PAR
+	Course: K1091
+	Author: Ivan Heimsath
+	Date: 20/04/15 00:16
+	Description: 
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int par;
+  cout<< "--------Ingreso de Numeros Pares--------\n\n";
+  cout<< "Ingrese valor para verificar si es Par: ";
+  cin>> par;
+  if (par%2==0){
+                 cout<< "\nEl Numero ingresado es Par";}
+           else{
+                 cout<< "\nEl Numero no es par, vuelva a ejecutar el programa";
+                }
+}
